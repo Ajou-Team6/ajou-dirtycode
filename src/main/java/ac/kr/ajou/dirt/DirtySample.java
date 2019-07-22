49ec493ab080d6aa0a19ae7808f4fd1c6b30a157
Jong in Park <whddls9632@naver.com>
2019-07-22 15:14:05 +0900

    Modify README.md

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -12,11 +12,6 @@ class DirtySample {
         this.items = items;
     }
 
-//    1. "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" 가 아니고, quality가 0보다 크면 quality 1 제거. sellin 1 감소 + Sellin이 0보다 작으면 quality 1 추가 제거.
-//    2. "Aged Brie"이고, quality가 50보다 작으면, quality 1 증가, sellin 1 제거. + Sellin이 0보다 작으면, quality 1 추가 증가. (quality는 최대 50)
-//    3. "Backstage passes to a TAFKAL80ETC concert"이고, quality가 50보다 작으면 quality 1 증가, sellin이 11보다 작으면 quality 추가로 1 증가,  6보다 작으면 추가로 1 증가, sellin 1 감소 + sellin이 0보다 작으면 quality = 0
-//    4. "Sulfuras, Hand of Ragnaros"이면, 이무것도 수행하지 않는다.
-//    Quality 범위는 0 ~ 50
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (isNotAgedBrieOrBackstage(items[i])) {
