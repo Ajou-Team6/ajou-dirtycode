e94f61c1c0576f0bf346d43a81915d3adefe1af0
younho9 <dusgh121@gmail.com>
2019-07-24 19:41:51 +0900

    extract name check method

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -17,72 +17,79 @@ class DirtySample {
             doIfNameIsAgedBrie(item);
             doIfNameIsBackstage(item);
             doIfNameIsNot3Cases(item);
-            ReduceSellInOneIfNameIsNotSulfuras(item);
-            DoIfSellInIsNegative(item);
         }
     }
 
     private void doIfNameIsNot3Cases(Item item) {
         if (!item.isNamed3Cases()) {
             doIfQualityIsPositive(item);
+            reduceSellInOne(item);
+            if(item.sellIn<0) {
+                doIfSellInIsNegative(item);
+            }
         }
     }
 
     private void doIfNameIsBackstage(Item item) {
         if (item.isNamed("Backstage passes to a TAFKAL80ETC concert")) {
-
+            doIfNameIsBackstageOrAged(item);
+            reduceSellInOne(item);
+            if(item.sellIn<0) {
+                doIfSellInIsNegative(item);
+            }
         }
     }
 
     private void doIfNameIsAgedBrie(Item item) {
         if (item.isNamed("Aged Brie")) {
-
-        }
-    }
-
-    private void DoIfSellInIsNegative(Item item) {
-        if(item.sellIn<0){
-            if (IfNameIsNotAgedBrieAndBackstage(item)) {
-                doIfQualityIsPositive(item);
-            }
-            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                item.quality=0;
-            }
-            else if(item.name.equals("Aged Brie")){
-                if(item.quality<50) {
-                    item.quality++;
-                }
+            doIfNameIsBackstageOrAged(item);
+            reduceSellInOne(item);
+            if(item.sellIn<0) {
+                doIfSellInIsNegative(item);
             }
         }
     }
 
-    private void ReduceSellInOneIfNameIsNotSulfuras(Item item) {
+    private void doIfSellInIsNegative(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            checkQualityUnderFiftyAndAddSome(item, 1);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros") && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void reduceSellInOne(Item item) {
         if(!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn=item.sellIn-1;
         }
     }
 
-
-
-    private void DoIfNameIsBackstageOrAged(Item item) {
-        if(item.quality<50){
-            item.quality++;
-            if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 6) item.quality = item.quality + 2;
-                else if (item.sellIn < 11) item.quality = item.quality + 1;
-            }
+    private void doIfNameIsBackstageOrAged(Item item) {
+        if (isItemNameBackstageAndSellinIsUnderWhat(item, 6)) {
+            checkQualityUnderFiftyAndAddSome(item, 3);
+        } else if (isItemNameBackstageAndSellinIsUnderWhat(item, 11)) {
+            checkQualityUnderFiftyAndAddSome(item, 2);
+        } else {
+            checkQualityUnderFiftyAndAddSome(item, 1);
         }
+    }
+
+    private void checkQualityUnderFiftyAndAddSome(Item item, int addWhat) {
+        if (item.quality < 50) {
+            item.quality = item.quality + addWhat;
+        }
+    }
+
+    private boolean isItemNameBackstageAndSellinIsUnderWhat(Item item, int threshold) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.sellIn < threshold;
     }
 
     private void doIfQualityIsPositive(Item item) {
-        if(item.quality > 0) {
+        if(item.quality>0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.quality = item.quality - 1;
         }
-    }
-
-    private boolean IfNameIsNotAgedBrieAndBackstage(Item item) {
-        return (!item.name.equals("Aged Brie") &&
-                !item.name.equals("Backstage passes to a TAFKAL80ETC concert"));
     }
 }
 
--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -1,6 +1,5 @@
 package ac.kr.ajou.dirt;
 
-import org.hamcrest.core.AnyOf;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
