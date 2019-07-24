bd0b1a8502560834da08c258e7ed2ef5532c85bb
Jong in Park <whddls9632@naver.com>
2019-07-24 17:42:33 +0900

    Reduce If Statements

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -16,26 +16,24 @@ class DirtySample {
         for (Item item : items) {
             if (IfNameIsNotAgedBrieAndBackstage(item)) {
                 DoIfQualityIsPositiveAndNameIsNotSulfuras(item);
-            } else DoIfNameIsBackstageOrAged(item);
+            } else {
+                DoIfNameIsBackstageOrAged(item);
+            }
             ReduceSellInOneIfNameIsNotSulfuras(item);
-            DoIfSellInIsNegative(item);
+            if(item.sellIn<0) {
+                DoIfSellInIsNegative(item);
+            }
         }
     }
 
 
     private void DoIfSellInIsNegative(Item item) {
-        if(item.sellIn<0){
-            if (IfNameIsNotAgedBrieAndBackstage(item)) {
-                DoIfQualityIsPositiveAndNameIsNotSulfuras(item);
-            }
-            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                item.quality=0;
-            }
-            else if(item.name.equals("Aged Brie")){
-                if(item.quality<50) {
-                    item.quality++;
-                }
-            }
+        if (item.name.equals("Aged Brie")) {
+            checkQualityUnderFiftyAndAddSome(item, 1);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros") && item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
@@ -45,16 +43,24 @@ class DirtySample {
         }
     }
 
-
-
     private void DoIfNameIsBackstageOrAged(Item item) {
-        if(item.quality<50){
-            item.quality++;
-            if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 6) item.quality = item.quality + 2;
-                else if (item.sellIn < 11) item.quality = item.quality + 1;
-            }
+        if (isItemNameBackstageAndSellinIsUnderWhat(item, 6)) {
+            checkQualityUnderFiftyAndAddSome(item, 3);
+        } else if (isItemNameBackstageAndSellinIsUnderWhat(item, 11)) {
+            checkQualityUnderFiftyAndAddSome(item, 2);
+        } else {
+            checkQualityUnderFiftyAndAddSome(item, 1);
         }
+    }
+
+    private void checkQualityUnderFiftyAndAddSome(Item item, int addWhat) {
+        if (item.quality < 50) {
+            item.quality = item.quality + addWhat;
+        }
+    }
+
+    private boolean isItemNameBackstageAndSellinIsUnderWhat(Item item, int threshold) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.sellIn < threshold;
     }
 
     private void DoIfQualityIsPositiveAndNameIsNotSulfuras(Item item) {
